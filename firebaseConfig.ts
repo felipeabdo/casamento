19325220e8277ea7d374c8f369de66c129da79e6
felipeabdo <felipe.abdo.ti@gmail.com>
2026-02-13 09:23:57 -0300

    Corrigindo conexao com banco de dados

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Se estiver rodando localmente e não funcionar, você pode colar as strings diretas aqui,
 // mas lembre-se de não publicar no GitHub com as senhas expostas se o repo for público.
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyADmbgE4Ih24wdEZ6b1zOILgpRwiE1eHkc",
+  authDomain: "casamento-ef122.firebaseapp.com",
+  projectId: "casamento-ef122",
+  storageBucket: "casamento-ef122.firebasestorage.app",
+  messagingSenderId: "83104445336",
+  appId: "1:83104445336:web:6f132dc1259deffe03f1ed",
+  measurementId: "G-DYVEKXFEF1"
 };
 
 // Inicializa o Firebase
